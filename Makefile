7f3fbaba79bd4f1fa1157209038c3320ece56f0f
oxygen <1973690778@qq.com>
2025-07-24 20:10:37 +0800

    feat:init

---
+++ Makefile
@@ -0,0 +1,4 @@
+build:
+	g++ -std=c++17 src/*.cpp -o ./bin/test
+run:
+	./bin/test
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+int main(){
+    std::cout<<"hello oxygen math"<<std::endl;
+    return 0;
+}
